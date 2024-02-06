d7da7c12c64ce21c297a1d9053a6fd060601dc5e
GuidoLippolis <guidolippolis2012@libero.it>
2024-02-06 11:50:25 +0100

    Aggiornamento script

--- scripts/QRadar-Attack.py
+++ scripts/QRadar-Attack.py
@@ -13,14 +13,16 @@ spiegazioni dei metodi
     - load_dataset(file_path): legge il dataset in formato csv, crea un header e restituisce
                                un dataframe
                                
-    - add_spaces(input_string): semplice metodo di formattazione della stringa relativa al campo
-                                DATA CAN (facoltativo). Aggiunge uno spazio dopo ogni blocco di
-                                4 bit della stringa di input
+    - add_spaces(input_string): facoltativo, semplice metodo di formattazione della stringa relativa al campo
+                                DATA CAN. Aggiunge uno spazio dopo ogni blocco di 4 bit della stringa di input
                                 
     - send_payload(payload, udp_socket, host_and_port): dato un payload in formato JSON, utilizza
                                                         udp_socket per inviarlo a host_and_port
                                                         
     - inject_payloads_from_dataset(path_to_dataset, log_file_name, start, end): contiene la logica dell'invio dei dati
+
+    - send_debug_payload(timestamp, id_can, data_can, label): costruisce un payload di test sulla base di timestamp, id_can
+                                                              e data_can e lo invia al SIEM
 
 '''
 
@@ -57,16 +59,6 @@ def load_dataset(file_path):
     
     return df
 
-
-def debug(file_path):
-    values_list = []
-    with open(file_path, 'r') as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            values_list.extend(row)
-    return values_list
-
-
 def add_spaces(input_string):
     
     result = ''
@@ -88,19 +80,41 @@ def send_payload(payload, udp_socket, host_and_port):
         
         print(f"Errore durante l'invio dei dati a QRadar: {str(e)}")
 
-def inject_payloads_from_dataset(path_to_dataset, log_file_name, start, end):
+def send_debug_payload(timestamp, id_can, data_can, label):
     
+    payload = {
+    
+        "UUID" : "63cd3082a04",
+        "eventID" : "CAN Payload",
+        "eventCategory" : "SpoofingGear",
+        "Timestamp" : timestamp,
+        "ID CAN" : id_can,
+        "DATA CAN" : add_spaces(data_can),
+        "label" : label
+        
+    }
+    
+    print(json.dumps(payload))
+    
+    send_payload(json.dumps(payload), udp_socket, host_and_port)
+
+def inject_payloads_from_dataset(path_to_dataset, log_file_name, start, end):
+
     df = load_dataset(path_to_dataset)
     
-    idCanR = read_csv('id_can_R.csv')
+    # idCanR = read_csv('id_can_R.csv')
     
     count_offenses = 0
+
+    count2 = 0
+    
+    # count_false_positive = 0
     
     if(end == 0):
         end = df.shape[0]
     
     with open(log_file_name, 'a', newline='') as log:
-    
+
         for index, row in df.iloc[start-1:end].iterrows():
             
             print('Sending payload N. ' + str(index + 1) + '...\t' + 'Class: ' + row[11])
@@ -109,30 +123,32 @@ def inject_payloads_from_dataset(path_to_dataset, log_file_name, start, end):
             
             if(row_values[11] == 'T'):
                 count_offenses += 1
-                
+
             payload = {
             
                 "UUID" : "63cd3082a04",
                 "eventID" : "CAN Payload",
-                "eventCategory" : "Fuzzing",
+                "eventCategory" : "SpoofingGear",
                 "Timestamp" : row_values[0],
                 "ID CAN" : row_values[1],
-                "DATA CAN" : add_spaces(row_values[3] + row_values[4] + row_values[5] + row_values[6] + row_values[7] + row_values[8] + row_values[9] + row_values[10])
+                "DATA CAN" : add_spaces(row_values[3] + row_values[4] + row_values[5] + row_values[6] + row_values[7] + row_values[8] + row_values[9] + row_values[10]),
+                "label" : row_values[11]
                 
             }
             
             json_payload = json.dumps(payload)
             
             print(json_payload)
-            
-            # send_payload(json_payload, udp_socket, host_and_port)
-            
+
             writer = csv.writer(log)
             writer.writerow(row_values)
-            
+
+            send_payload(json_payload, udp_socket, host_and_port)
+
             print('-----------------------------------------------------------------------------------------------------------------------------------------')
-    
+        
     print('Total offenses = ' + str(count_offenses))
+    # print('Total false positive = ' + str(count_false_positive))
         
     udp_socket.close()
 
@@ -143,9 +159,15 @@ try:
     parser.add_argument('-d', help = 'Path to the dataset', required = True)
     parser.add_argument('-end', help = 'Number of payloads to send (if 0, the whole dataset will be processed)', required = True)
     parser.add_argument('-start', help = 'Start from payload n. ?', required = True)
-    parser.add_argument('-log', help = 'Name of the log file', required = True)
+    parser.add_argument('-log', help = 'Name of the log file (in .csv format)', required = False)
     
     args = parser.parse_args()
+    
+    # Spoofing RPM
+    # send_debug_payload(1478195722.800121, '0316', '452924ff292400ff', 'T') 
+
+    # Spoofing Gear
+    # send_debug_payload(1478195722.800121, '043f', '014560ff6b000000', 'T') 
     
     inject_payloads_from_dataset(args.d, args.log, int(args.start), int(args.end))
     
